9a41f51cde132d7fac5d7120252b22b5793ccb8f
Alemorar <infoserv100@gmail.com>
2015-11-20 13:10:04 -0300

    actualizando

--- src/java/biblioteca/datos/hibernate/dao/IUsuarioDAO.java
+++ src/java/biblioteca/datos/hibernate/dao/IUsuarioDAO.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package biblioteca.datos.hibernate.dao;
 
 import biblioteca.modelo.dominio.Usuario;
 
-/**
- *
- * @author alejandro
- */
 public interface IUsuarioDAO {
     void agregarUsuario(Usuario usuario);
 }
