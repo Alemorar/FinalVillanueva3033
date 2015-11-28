e2940b064d8dd677908de2577f9e70afdfd0f16b
Alemorar <infoserv100@gmail.com>
2015-11-28 02:30:29 -0300

    Agregamos Loggin

---
+++ src/java/biblioteca/beans/forms/LogginFormBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca.beans.forms;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author alejandro
+ */
+@ManagedBean
+@RequestScoped
+public class LogginFormBean {
+
+    /**
+     * Creates a new instance of LogginFormBean
+     */
+    public LogginFormBean() {
+    }
+    
+}
